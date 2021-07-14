47aafef585dc224f63bf37649e902ff79e8252e1
yangman <yangman945@163.com>
2021-07-15 00:10:00 +0800

    feat: 侧边导航栏

--- src/layout/components/Sidebar/Item.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>导航四</template>
-    </el-menu-item>
-</template>
---
+++ src/layout/components/SidebarItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- el-submenu 分组  el-menu-item 根节点 -->
+        <el-submenu index="1">
+            <template #title>
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+            </template>
+            <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+                <template #title>选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+        </el-submenu>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: "SidebarItem"
+})
+</script>
